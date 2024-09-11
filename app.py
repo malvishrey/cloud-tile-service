848689f507870f3bdd57780cedf8cbb7da49a8a8
Shrey Malvi <kakashi@Shreys-MacBook-Air.local>
2024-09-11 13:22:55 -0700

    adde logging

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 # server.py
 from flask import Flask
 from main import update_txt_files  # Import the function from your script
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 @app.route("/", methods=['POST'])
 def index():
-    print('reach')
+    logger.info(f"Received here!!!!")
     update_txt_files()  # Run the task
     return "Task completed", 200
 
